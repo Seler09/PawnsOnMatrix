2e2385f37ae6001b77f2074d220b266526aa18de
Seler09 <blackbird092@gmail.com>
2017-06-05 14:53:17 +0200

    Final and wroking

--- main.cpp
+++ main.cpp
@@ -9,13 +9,15 @@
 
 using namespace std;
 
-std::mutex mx;
+std::mutex mPX;
+std::mutex mPY;
 
 std::queue<int>kolejka1;
 std::queue<int>kolejka2;
+std::mutex k1;
+std::mutex k2;
+std::mutex mx;
 
-std::vector<int>x;
-std::vector<int>y;
 
 int sizeX=0;
 int sizeY=0;
@@ -23,35 +25,162 @@ int sizeY=0;
 int pX;
 int pY;
 
-int kolumna=0;
-int ala=0;
+int pDodawania;
+int pPrzesowania;
+
+
+
+struct pionek
+{
+	int x;
+	int y;
+};
+
+pionek tenPionek;
+
+std::vector<pionek>wspPionka;
+
+
+//--------------------FUNKCJE------------------------
+
+
 
 void producent()
 {
-
-	int wspX = rand()% (sizeX-1) + 0;
-	mx.lock();
+	do{
+	int wspX = rand()% (sizeX) + 0;
+	k1.lock();
 	kolejka1.push(wspX);
-	mx.unlock();
+	k1.unlock();
 
-	int wspY = rand()% (sizeY-1) + 0;
-	mx.lock();
+	int wspY = rand()% (sizeY) + 0;
+	k2.lock();
 	kolejka2.push(wspY);
-	mx.unlock();
-
+	k2.unlock();
+	}while(true);
 }
 
 void przetwarzacz()
 {
-	mx.lock();
-	pX = kolejka1.front();
-	kolejka1.pop();
-	mx.unlock();
+	do{
+		mx.lock();
+			k1.lock();
+				tenPionek.x = kolejka1.front();
+				kolejka1.pop();
+			k1.unlock();
 
-	mx.lock();
-	pY = kolejka2.front();
-	kolejka2.pop();
-	mx.unlock();
+			k2.lock();
+				tenPionek.y = kolejka2.front();
+				kolejka2.pop();
+			k2.unlock();
+		mx.unlock();
+		std::this_thread::sleep_for (std::chrono::milliseconds(100));
+	}while(true);
+}
+
+void rysowaniePionka()
+{
+	do{
+		mx.lock();
+			mvaddch(tenPionek.y,((tenPionek.x)*2)+1,'i');
+
+			mPX.lock();
+				wspPionka.push_back(tenPionek);
+			mPX.unlock();
+		mx.unlock();
+
+		refresh();
+		std::this_thread::sleep_for (std::chrono::seconds(pDodawania));
+	}while(true);
+
+}
+
+void przesowaniePionka()
+{
+	do{
+
+		mPX.lock();
+			for(int i=0;i<wspPionka.size();i++)
+			{
+				int ruch = rand()% (4) + 0;
+
+				if(ruch==0)//dól
+				{
+					mvaddch(wspPionka[i].y,((wspPionka[i].x)*2)+1,'_');
+
+					if(wspPionka[i].y==sizeY-1)
+					{
+						wspPionka[i].y=wspPionka[i].y-1;
+					}
+					else
+					{
+						wspPionka[i].y=wspPionka[i].y+1;
+					}
+					mvaddch(wspPionka[i].y,((wspPionka[i].x)*2)+1,'i');
+
+					refresh();
+
+				}
+
+				if(ruch==1)//góra
+				{
+					mvaddch(wspPionka[i].y,((wspPionka[i].x)*2)+1,'_');
+
+					if(wspPionka[i].y==0)
+					{
+						wspPionka[i].y=wspPionka[i].y+1;
+					}
+					else
+					{
+						wspPionka[i].y=wspPionka[i].y-1;
+					}
+					mvaddch(wspPionka[i].y,((wspPionka[i].x)*2)+1,'i');
+
+					refresh();
+
+				}
+
+				if(ruch==2)//lewo
+				{
+					mvaddch(wspPionka[i].y,((wspPionka[i].x)*2)+1,'_');
+
+					if(wspPionka[i].x==0)
+					{
+						wspPionka[i].x=wspPionka[i].x+1;
+					}
+					else
+					{
+						wspPionka[i].x=wspPionka[i].x-1;
+					}
+					mvaddch(wspPionka[i].y,((wspPionka[i].x)*2)+1,'i');
+
+					refresh();
+
+				}
+
+				if(ruch==3)//prawo
+				{
+					mvaddch(wspPionka[i].y,((wspPionka[i].x)*2)+1,'_');
+
+					if(wspPionka[i].x==sizeX-1)
+					{
+						wspPionka[i].x=wspPionka[i].x-1;
+					}
+					else
+					{
+						wspPionka[i].x=wspPionka[i].x+1;
+					}
+					mvaddch(wspPionka[i].y,((wspPionka[i].x)*2)+1,'i');
+
+					refresh();
+
+				}
+
+			}
+		mPX.unlock();
+		std::this_thread::sleep_for (std::chrono::milliseconds(pPrzesowania));
+	}while(true);
+
 }
 
 void macierz()
@@ -70,46 +199,74 @@ void macierz()
 			}
 			else
 			{
-				if(rzad==pY&&kolumna==((pX*2)+1))
-				{
-					mvaddch(rzad,kolumna,'o');
-					cout<<kolumna;
-					cout<<rzad;
-					cout<<pX;
-					cout<<"\n\n";
-				}
-				else
-					mvaddch(rzad,kolumna,'_');
+				mvaddch(rzad,kolumna,'_');
 			}
 
 		}
 	}
-	getch();
-	endwin();
+
+
+
+
 }
 
 
+void menu();
 
+
+//--------------------MAIN------------------------------
 int main() {
 
 	srand(time(NULL));
 
-	sizeX = 10;
-	sizeY = 10;
-	//cin>>pX;
-	//cin>>pY;
-	for(int lel=0;lel<100;lel++)
-	{
-	producent();
-	przetwarzacz();
-	macierz();
-	}
-	cout<<endl<<"pX po operacji: "<<pX<<endl;
+	menu();
 
-	cout<<endl;
+    macierz();
 
+    auto tProducent = thread(producent);
+    auto tPrzetwarzacz = thread(przetwarzacz);
+    auto tRysowanie = thread(rysowaniePionka);
+    auto tRelokacja = thread(przesowaniePionka);
 
+    tProducent.join();
+    tPrzetwarzacz.join();
+    tRysowanie.join();
+    tRelokacja.join();
+
+    endwin();
 
 
 	return 0;
+}
+
+
+void menu()
+{
+	int wybor;
+		cout<<"MENU\n";
+		cout<<"[1] Wpisz parametry.\n";
+		cout<<"[2] Automatyczne parametry.\n";
+		cin>>wybor;
+		cout<<endl;
+
+		switch(wybor){
+		case 1:
+			cout << "Podaj szerkosc macierzy: ";
+			cin >> sizeX;
+			cout << "Podaj wysokosc macierzy: ";
+			cin >> sizeY;
+			cout << "Podaj predkosc dodawania i przesowania pionkow [sek]: ";
+			cin >> pDodawania;
+			cin >> pPrzesowania;
+			break;
+		case 2:
+			sizeX=10;
+			sizeY=10;
+			pDodawania=1;
+			pPrzesowania=1000;
+			break;
+		default:
+			cout<<"Error\n";
+			break;
+		}
 }
